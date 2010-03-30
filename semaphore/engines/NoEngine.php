81e20e6a520a57384855089d840102679d8f7e36
TomFrost <tom@frosteddesign.com>
2010-03-30 15:52:20 -0400

    Fixing bug causing 'NoEngine' to throw an error on instantiation

--- semaphore/engines/NoEngine.php
+++ semaphore/engines/NoEngine.php
@@ -1,9 +1,13 @@
 <?php
 namespace hydrogen\semaphore\engines;
 
-use \hydrogen\semaphore\SemaphoreEngine;
+use hydrogen\semaphore\SemaphoreEngine;
 
 class NoEngine extends SemaphoreEngine {
+	public function __construct() {
+		parent::__construct();
+	}
+	
 	public function acquire($sem_name, $wait_time=2) {
 		return true;
 	}
