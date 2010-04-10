57df8842f58a605031fa295860638a95214118c4
TomFrost <tom@frosteddesign.com>
2010-04-10 21:40:01 +0800

    Competely reworked the logic.  There are now two functions, getCustomEngine and getEngine.  getCustomEngine is the only function that accepts connection parameters.  getEngine will get the default engine, or the specified one if a sub-key name is passed in.

--- database/DatabaseEngineFactory.php
+++ database/DatabaseEngineFactory.php
@@ -26,10 +26,11 @@ class DatabaseEngineFactory {
 	private function __construct() {}
 	
 	/**
-	 * Creates a new instance of this DatabaseEngine if the specified connection is new, or returns
-	 * the stored engine for a connection that an engine's already been instantiated for.  The
-	 * DatabaseEngine is connected as soon as it is created.
+	 * Creates a DatabaseEngine with the specified connection parameters.  If a DatabaseEngine
+	 * was created using the exact same parameters provided any time earlier in this page request,
+	 * that same DatabaseEngine will be returned rather than creating a new one.
 	 *
+	 * @param string engineName The name of the engine to use for this connection.
 	 * @param string|boolean host Hostname to connect to, or <code>false</code> to use a the
 	 * 		hostname defined in the Config object.
 	 * @param int|boolean port Port to connect to, or <code>false</code> to use the port defined
@@ -42,8 +43,6 @@ class DatabaseEngineFactory {
 	 * 		use the username defined in the Config object.
 	 * @param string|boolean password The password to connect with, or <code>false</code> to
 	 * 		use the password defined in the Config object.
-	 * @param string|boolean engine The name of the engine to use for this connection, or
-	 * 		<code>false</code> to use the engine defined in the Config object.
 	 * @param string|boolean tablePrefix The prefix for tables, or <code>false</code> if no prefix
 	 *		is required
 	 * @throws hydrogen\database\exceptions\DatabaseConnectionException if a connection could not
@@ -51,83 +50,79 @@ class DatabaseEngineFactory {
 	 * @return DatabaseEngine The requested DatabaseEngine with a connection to the specified
 	 * 		database server.
 	 */
-	public static function getEngine($host=false, $port=false, $socket=false,
-			$database=false, $username=false, $password=false, $engine=false, $tablePrefix=false) {
-		$dbSelectKey = Config::getRequiredVal('database','engine');
-		if(is_array($dbSelectKey)){
-			$dbSelectKey = array_keys($dbSelectKey);
-			$dbSelect = $dbSelectKey[0];
-		}
-		else 
-			$dbSelect = false;
-		$host = $host ?: Config::getVal('database', 'host', $dbSelect);
-		$port = $port ?: Config::getVal('database', 'port', $dbSelect);
-		$socket = $socket ?: Config::getVal('database', 'socket', $dbSelect);
-		$database = $database ?: Config::getVal('database', 'database', $dbSelect);
-		$username = $username ?: Config::getVal('database', 'username', $dbSelect);
-		$password = $password ?: Config::getVal('database', 'password', $dbSelect);
-		$tablePrefix = $tablePrefix ?: Config::getVal('database', 'table_prefix', $dbSelect);
-		$engine = $engine ?: Config::getRequiredVal('database', 'engine', $dbSelect);
-		if (strpos($engine, 'hydrogen') !== false)
-			$engineClass = &$engine;
+	public static function getCustomEngine($engineName, $host=false, $port=false, $socket=false,
+			$database=false, $username=false, $password=false, $tablePrefix=false) {
+		if (strpos($engineName, 'hydrogen') !== false)
+			$engineClass = &$engineName;
 		else
-			$engineClass = '\hydrogen\database\engines\\' . $engine . 'Engine';
-		$key = ($host ?: '') . ':' . ($port ?: '') . ':' . ($socket ?: '') . ':' . 
-			($database ?: '') . ':' . ($username ?: '') . ':' . ($engineClass ?: '') . ':' . ($tablePrefix ?: '');
+			$engineClass = '\hydrogen\database\engines\\' . $engineName . 'Engine';
+			
+		$key = $host . ':' . $port . ':' . $socket . ':' . $database . ':' . $username .
+			':' . $engineClass  . ':' . $tablePrefix;
+		
 		if (!isset(static::$engine[$key])) {
-			static::$engine[$key] = new $engineClass(
-				$host ?: Config::getVal('database', 'host', $dbSelect),
-				$port ?: Config::getVal('database', 'port', $dbSelect),
-				$socket ?: Config::getVal('database', 'socket', $dbSelect),
-				$database ?: Config::getVal('database', 'database', $dbSelect),
-				$username ?: Config::getVal('database', 'username', $dbSelect),
-				$password ?: Config::getVal('database', 'password', $dbSelect),
-				$tablePrefix ?: Config::getVal('database', 'table_prefix', $dbSelect)
-				);
+			static::$engine[$key] = new $engineClass($host, $port, $socket, $database,
+				$username, $password, $tablePrefix);
 		}
 		return static::$engine[$key];
 	}
 	
 	/**
-	 * Creates a new instance of this DatabaseEngine if the specified connection is new, or returns
+	 * Creates a new instance of DatabaseEngine if the specified connection is new, or returns
 	 * the stored engine for a connection that an engine's already been instantiated for.  The
 	 * DatabaseEngine is connected as soon as it is created.
+	 * 
+	 * If no dbConfigName is specified, the database configuration from the
+	 * {@link \hydrogen\config\Config} object is used.  If there are multiple database configurations
+	 * in the Config object, the first one defined will be used here.
 	 *
-	 * @param string dbSelect Subkey value to call for database configuration
-	 * @param string|boolean tablePrefix The prefix for tables, or <code>false</code> to use prefix
-	 *		defined in the Config object
+	 * If multiple database configurations have been specified in the Config object, the sub-key of
+	 * the appropriate configuration can be passed in for dbConfigName.  For example:
+	 * <pre>
+	 * [database]
+	 * host[primary] = localhost
+	 * port[primary] = 3306
+	 * socket[primary] = 
+	 * database[primary] = myDB
+	 * username[primary] = myDBUser
+	 * password[primary] = myDBPass
+	 * table_prefix[primary] = myApp_
+	 *
+	 * host[backup] = backup.mydomain.com
+	 * port[backup] = 3306
+	 * socket[backup] = 
+	 * database[backup] = backupDB
+	 * username[backup] = myDBUser
+	 * password[backup] = myDBPass
+	 * table_prefix[backup] = myApp_
+	 * </pre>
+	 * Using this configuration, this function could be called with either "primary" or "backup"
+	 * as the dbConfigName argument to get the appropriate DatabaseEngine.  If no dbConfigName
+	 * is specified, the "primary" sub-key will be used since it appears first in the file.
+	 *
+	 * @param string dbConfigName The subkey of the engine configuration to pull from the
+	 * 		{@link \hydrogen\config\Config} object.
 	 * @throws hydrogen\database\exceptions\DatabaseConnectionException if a connection could not
 	 * 		be made.
 	 * @return DatabaseEngine The requested DatabaseEngine with a connection to the specified
 	 * 		database server.
 	 */
-	public static function getEngineByName($dbSelect, $tablePrefix=false) {
-		$host = Config::getVal('database', 'host', $dbSelect);
-		$port = Config::getVal('database', 'port', $dbSelect);
-		$socket = Config::getVal('database', 'socket', $dbSelect);
-		$database = Config::getVal('database', 'database', $dbSelect);
-		$username = Config::getVal('database', 'username', $dbSelect);
-		$password = Config::getVal('database', 'password', $dbSelect);
-		$tablePrefix = $tablePrefix ?: Config::getVal('database', 'table_prefix', $dbSelect);
-		$engine = Config::getRequiredVal('database', 'engine', $dbSelect);
-		if (strpos($engine, 'hydrogen') !== false)
-			$engineClass = &$engine;
-		else
-			$engineClass = '\hydrogen\database\engines\\' . $engine . 'Engine';
-		$key = ($host ?: '') . ':' . ($port ?: '') . ':' . ($socket ?: '') . ':' . 
-			($database ?: '') . ':' . ($username ?: '') . ':' . ($engineClass ?: '') . ':' . ($tablePrefix ?: '');
-		if (!isset(static::$engine[$key])) {
-			static::$engine[$key] = new $engineClass(
-				$host ?: Config::getVal('database', 'host', $dbSelect),
-				$port ?: Config::getVal('database', 'port', $dbSelect),
-				$socket ?: Config::getVal('database', 'socket', $dbSelect),
-				$database ?: Config::getVal('database', 'database', $dbSelect),
-				$username ?: Config::getVal('database', 'username', $dbSelect),
-				$password ?: Config::getVal('database', 'password', $dbSelect),
-				$tablePrefix ?: Config::getVal('database', 'table_prefix', $dbSelect)
-				);
+	public static function getEngine($dbConfigName=false) {
+		if ($dbConfigName === false) {
+			$engines = Config::getRequiredVal('database','engine');
+			if (is_array($engines))
+				$dbConfigName = (array_keys($engines))[0];
 		}
-		return static::$engine[$key];
+			
+		return static::getCustomEngine(
+			Config::getRequiredVal('database', 'engine', $dbConfigName),
+			Config::getVal('database', 'host', $dbConfigName),
+			Config::getVal('database', 'port', $dbConfigName),
+			Config::getVal('database', 'socket', $dbConfigName),
+			Config::getVal('database', 'database', $dbConfigName),
+			Config::getVal('database', 'username', $dbConfigName),
+			Config::getVal('database', 'password', $dbConfigName)
+			);
 	}
 }
 
