5d593d81dc0474681f84aeb4590d69a0ae4831ed
TomFrost <tom@frosteddesign.com>
2010-04-28 23:08:57 -0400

    Allowing persistent connections in MemcachedEngine; restricting keys so they cannot have spaces.

--- cache/engines/MemcachedEngine.php
+++ cache/engines/MemcachedEngine.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * MemcachedEngine.php
- * by magik
+ * Copyright (c) 2009 - 2010, Frosted Design
+ * All rights reserved.
  */
 
 namespace hydrogen\cache\engines;
@@ -11,62 +11,49 @@ use hydrogen\config\Config;
 use hydrogen\cache\CacheEngine;
 
 /**
- * CacheEngine to interface with the Memcache server.  It uses the 'memcached'
+ * CacheEngine to interface with the Memcache server.  It uses the 'Memcached'
  * PECL extension.
  *
- * @requires memcached
+ * @requires Memcached
  */
 class MemcachedEngine implements CacheEngine {
 	protected $memcached;
 	
 	public function __construct() {
-		$res = new Memcached();
-		$host = Config::getVal('cache', 'memcache_host') ?: 'localhost';
-		$port = Config::getVal('cache', 'memcache_port') ?: 11211;
-		$res->addServer($host, $port);
-		$this->memcached = $res;
-	}
-	
-//	public function __destruct() {
-//		$this->memcached->close();
-//	}
-
-	public static function fixKeyWhiteSpace($key) {
-		return str_replace(" ", "_", $key);
+		$mc = new Memcached(Config::getVal("cache", "pool_name"));
+		if (count($mc->getServerList()) === 0) {
+			$host = Config::getVal('cache', 'memcache_host') ?: 'localhost';
+			$port = Config::getVal('cache', 'memcache_port') ?: 11211;
+			$mc->addServer($host, $port);
+		}
+		$this->memcached = $mc;
 	}
 	
 	public function add($key, $value, $ttl) {
-		$key = self::fixKeyWhiteSpace($key);
 		return $this->memcached->add($key, $value, $ttl);
 	}
 	
 	public function replace($key, $value, $ttl) {
-		$key = self::fixKeyWhiteSpace($key);
 		return $this->memcached->replace($key, $value, $ttl);
 	}
 	
 	public function get($key) {
-		$key = self::fixKeyWhiteSpace($key);
 		return $this->memcached->get($key);
 	}
 
 	public function set($key, $value, $ttl) {
-		$key = self::fixKeyWhiteSpace($key);
 		return $this->memcached->set($key, $value, $ttl);
 	}
 
 	public function increment($key, $value=1) {
-		$key = self::fixKeyWhiteSpace($key);
 		return $this->memcached->increment($key, $value);
 	}
 
 	public function decrement($key, $value=1) {
-		$key = self::fixKeyWhiteSpace($key);
 		return $this->memcached->decrement($key, $value);
 	}
 	
 	public function delete($key) {
-		$key = self::fixKeyWhiteSpace($key);
 		return $this->memcached->delete($key, 0);
 	}
 	
