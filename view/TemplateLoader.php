cd2f9a7ee91ada6e08d516dee14544a0a7d4f04e
TomFrost <tom@frosteddesign.com>
2010-08-03 10:23:09 -0400

    Adding some documentation.

--- view/TemplateLoader.php
+++ view/TemplateLoader.php
@@ -8,13 +8,27 @@ namespace hydrogen\view;
 
 use hydrogen\view\exceptions\NoSuchViewException;
 
+/**
+ * TemplateLoader is responsible for the reading, parsing, caching, and
+ * rendering of Hdyrogen templates.  Heavily modeled after Django.
+ */
 class TemplateLoader {
 	private $templatePath;
 	
+	/**
+	 * Creates a new template loader for the given template.
+	 *
+	 * @param string templatePath The absolute path to the template to be
+	 * 		loaded.
+	 */
 	public function __construct($templatePath) {
 		$this->templatePath = $templatePath;
 	}
 	
+	/**
+	 * Show the rendered view, caching it or reading it from the cache if
+	 * appropriate.
+	 */
 	public function display() {
 		
 	}
