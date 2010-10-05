b8a55b56740827e4db2a0102fcfc1ab1e329cc10
TomFrost <tom@frosteddesign.com>
2010-10-05 15:14:48 -0400

    Whoops, forgot to add the TypedValue class to the EE Parser commit.

---
+++ common/TypedValue.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * Copyright (c) 2009 - 2010, Frosted Design
+ * All rights reserved.
+ */
+
+namespace hydrogen\common;
+
+/**
+ * TypedValue is simply a resource-friendly method of storing a value and
+ * some sort of identifier that describes it, rather than using a much
+ * more expensive array for such a task.
+ */
+class TypedValue {
+
+	/**
+	 * The type of variable stored in $value.  This is arbitrary and can
+	 * be used however it's required.
+	 */
+	public $type;
+
+	/**
+	 * The value of type $type.  This is arbitrary and can be used however
+	 * it's required.
+	 */
+	public $value;
+
+	/**
+	 * Creates a new TypedValue.  The type and value passed in the arguments
+	 * will become available in the $type and $value member variables.
+	 */
+	public function __construct($type, $value) {
+		$this->type = &$type;
+		$this->value = &$value;
+	}
+
+	/**
+	 * When treated as a string, a TypedValue will default to its value's
+	 * string representation.
+	 */
+	public function __toString() {
+		return $this->value;
+	}
+}
+
+?>
