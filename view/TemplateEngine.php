8c8b1b5177c6ebf726e28c649967dde9cbc500a9
TomFrost <tom@frosteddesign.com>
2010-12-06 18:50:36 -0500

    Changing Template to a TemplateEngine interface.

---
+++ view/TemplateEngine.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Copyright (c) 2009 - 2010, Frosted Design
+ * All rights reserved.
+ */
+
+namespace hydrogen\view;
+
+interface TemplateEngine {
+
+	public static function getPHP($loader, $templateName);
+
+}
+
+?>
