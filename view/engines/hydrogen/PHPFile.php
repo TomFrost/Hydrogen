6ad3885f7b296b7ea93141edd3001db9b5228a24
TomFrost <tom@frosteddesign.com>
2010-12-06 23:19:57 -0500

    Laying the groundwork for PHP rendering.

---
+++ view/engines/hydrogen/PHPFile.php
@@ -0,0 +1,72 @@
+<?php
+/*
+ * Copyright (c) 2009 - 2010, Frosted Design
+ * All rights reserved.
+ */
+
+namespace hydrogen\view;
+
+use hydrogen\view\exceptions\MemberAlreadyExistsException;
+
+class PHPFile {
+	const PHP_OPENTAG = "<?php ";
+	const PHP_CLOSETAG = " ?>";
+
+	protected $contextDeclarations;
+	protected $privateDeclarations;
+	protected $functions;
+	protected $content;
+
+	public function __construct() {
+		$this->contextDeclarations = array();
+		$this->privateDeclarations = array();
+		$this->functions = array();
+		$this->content = '';
+	}
+
+	public function addContextDeclaration($name, $value) {
+		if (isset($this->contextDeclarations[$name]))
+			throw new MemberAlreadyExistsException(
+				"A context variable named '$name' has already been declared.");
+		$this->contextDeclarations[$name] = $value;
+	}
+
+	public function addPrivateDeclaration($name, $value) {
+		if (isset($this->privateDeclarations[$name]))
+			throw new MemberAlreadyExistsException(
+				"A private variable named '$name' has already been declared.");
+		$this->privateDeclarations[$name] = $value;
+	}
+
+	public function addFunction($name, $args, $code) {
+		if (isset($this->functions[$name]))
+			throw new MemberAlreadyExistsException(
+				"A function named '$name' has already been declared.");
+		$this->functions[$name] = array($args, $code);
+	}
+
+	public function addPageContent($mixed) {
+		$this->content .= $mixed;
+	}
+
+	public function getPHP() {
+		$page = self::PHP_OPENTAG;
+		foreach ($contextDeclarations as $var => $val)
+			$page .= '$context->' . $var . " = $val;";
+		foreach ($privateDeclarations as $var => $val)
+			$page .= '$' . $var . " = $val;";
+		foreach ($functions as $name => $data) {
+			$page .= "function $name(";
+			if (is_array($data[0]))
+				$page .= implode(', ', $data[0]);
+			$page .= ") {$data[1]}";
+		}
+		$page .= self::PHP_CLOSETAG . $this->content;
+		$page = str_replace(self::PHP_CLOSETAG . self::PHP_OPENTAG, '', $page);
+		return $page;
+	}
+
+	public function writePHPFile($filePath) {}
+}
+
+?>
