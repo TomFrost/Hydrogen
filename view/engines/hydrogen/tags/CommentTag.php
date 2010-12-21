89e16d35322d3a5dd5b6b70b6c1b91a663c68803
TomFrost <tom@frosteddesign.com>
2010-12-21 09:45:03 -0500

    Added the Comment tag.

---
+++ view/engines/hydrogen/tags/CommentTag.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Copyright (c) 2009 - 2010, Frosted Design
+ * All rights reserved.
+ */
+
+namespace hydrogen\view\engines\hydrogen\tags;
+
+use hydrogen\view\engines\hydrogen\Tag;
+use hydrogen\view\engines\hydrogen\nodes\BlockNode;
+
+class CommentTag extends Tag {
+
+	public static function getNode($cmd, $args, $parser, $origin) {
+		$nodes = $parser->parse("endcomment");
+		$parser->skipNextToken();
+		return false;
+	}
+
+}
+
+?>
