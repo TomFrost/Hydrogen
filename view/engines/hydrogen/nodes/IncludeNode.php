a7be50bb00202d73964676143a185523cf930ba2
TomFrost <tom@frosteddesign.com>
2010-12-23 13:42:47 -0500

    Changing the include tag so a hardcoded string OR dynamic variable can be used.

---
+++ view/engines/hydrogen/nodes/IncludeNode.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Copyright (c) 2009 - 2010, Frosted Design
+ * All rights reserved.
+ */
+
+namespace hydrogen\view\engines\hydrogen\nodes;
+
+use hydrogen\view\engines\hydrogen\Node;
+use hydrogen\view\engines\hydrogen\PHPFile;
+
+class IncludeNode implements Node {
+	protected $variable;
+
+	public function __construct($variable) {
+		$this->variable = &$variable;
+	}
+
+	public function render($phpFile) {
+		$phpFile->addPageContent(PHPFile::PHP_OPENTAG . '$this->loadView(' .
+			$this->variable->getVariablePHP($phpFile) . ');' .
+			PHPFile::PHP_CLOSETAG);
+	}
+}
+
+?>
--- view/engines/hydrogen/tags/IncludeTag.php
+++ view/engines/hydrogen/tags/IncludeTag.php
@@ -7,18 +7,27 @@
 namespace hydrogen\view\engines\hydrogen\tags;
 
 use hydrogen\view\engines\hydrogen\Tag;
+use hydrogen\view\engines\hydrogen\Lexer;
+use hydrogen\view\engines\hydrogen\nodes\IncludeNode;
+use hydrogen\view\engines\hydrogen\nodes\VariableNode;
 use hydrogen\view\engines\hydrogen\exceptions\TemplateSyntaxException;
 
 class IncludeTag extends Tag {
 
 	public static function getNode($cmd, $args, $parser, $origin) {
-		if (empty($args)) {
+		if (empty($args) || strpos($args, ' ') !== false) {
 			throw new TemplateSyntaxException(
 				'Tag "include" in template "' . $origin .
-				'" requires a template name as an argument.');
+				'" requires a single template name as an argument.');
 		}
-		$parser->prependPage($args);
-		return false;
+		if (Lexer::surroundedBy($args, '"', '"')) {
+			$parser->prependPage(substr($args, 1, -1));
+			return false;
+		}
+		$token = Lexer::getVariableToken($origin, $args);
+		$node = new VariableNode($token->varLevels, $token->filters,
+			false, $origin);
+		return new IncludeNode($node);
 	}
 
 }
