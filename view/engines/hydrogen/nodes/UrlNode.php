68588d400f45db10609435ea021b773534bbd034
TomFrost <tom@frosteddesign.com>
2010-12-24 09:45:19 -0500

    Added the 'url' tag/node pair.

---
+++ view/engines/hydrogen/nodes/UrlNode.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * Copyright (c) 2009 - 2010, Frosted Design
+ * All rights reserved.
+ */
+
+namespace hydrogen\view\engines\hydrogen\nodes;
+
+use hydrogen\view\engines\hydrogen\ExpressionParser;
+use hydrogen\view\engines\hydrogen\Node;
+use hydrogen\view\engines\hydrogen\exceptions\NoSuchFilterException;
+use hydrogen\view\engines\hydrogen\exceptions\NoSuchVariableException;
+use hydrogen\view\engines\hydrogen\Lexer;
+use hydrogen\view\engines\hydrogen\PHPFile;
+use hydrogen\config\Config;
+
+class UrlNode implements Node {
+	protected $folders;
+	protected $kvPairs;
+
+	public function __construct($folders, $kvPairs) {
+		$this->folders = $folders;
+		$this->kvPairs = $kvPairs;
+	}
+
+	public function render($phpFile) {
+		$appURL = Config::getRequiredVal('general', 'app_url');
+		while ($appURL[strlen($appURL) - 1] === '/')
+			$appURL = substr($appURL, 0, -1);
+		foreach ($this->folders as $folder) {
+			if (is_object($folder)) {
+				$appURL .= '/' . PHPFile::PHP_OPENTAG . 'echo ' .
+					$folder->getVariablePHP($phpFile) . ';' .
+					PHPFile::PHP_CLOSETAG;
+			}
+			else
+				$appURL .= '/' . $folder;
+		}
+		if ($this->kvPairs) {
+			if (strtolower(substr($appURL, -4)) !== '.php')
+				$appURL .= '/';
+			$appURL .= '?';
+			foreach ($this->kvPairs as $key => $val) {
+				$appURL .= $key . '=';
+				if (is_object($val)) {
+					$appURL .= PHPFile::PHP_OPENTAG . 'echo ' .
+						$val->getVariablePHP($phpFile) . ';' .
+						PHPFile::PHP_CLOSETAG;
+				}
+				else
+					$appURL .= $val;
+				$appURL .= '&';
+			}
+			$appURL = substr($appURL, 0, -1);
+		}
+		$phpFile->addPageContent($appURL);
+	}
+}
+
+?>
---
+++ view/engines/hydrogen/tags/UrlTag.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * Copyright (c) 2009 - 2010, Frosted Design
+ * All rights reserved.
+ */
+
+namespace hydrogen\view\engines\hydrogen\tags;
+
+use hydrogen\view\engines\hydrogen\Lexer;
+use hydrogen\view\engines\hydrogen\Tag;
+use hydrogen\view\engines\hydrogen\nodes\UrlNode;
+use hydrogen\view\engines\hydrogen\nodes\VariableNode;
+use hydrogen\view\engines\hydrogen\exceptions\TemplateSyntaxException;
+
+class UrlTag extends Tag {
+
+	public static function getNode($cmd, $args, $parser, $origin) {
+		$tokens = preg_split('/[\s\/]/', $args, null, PREG_SPLIT_NO_EMPTY);
+		$folders = array();
+		$kvPairs = array();
+		foreach ($tokens as $token) {
+			if (strpos($token, '=') !== false) {
+				$keyVal = explode('=', $token);
+				if (count($keyVal) !== 2 || !$keyVal[0] || !$keyVal[1]) {
+					throw new TemplateSyntaxException(
+						'URL tag parameter in template "' . $origin .
+						'" contains invalid key=value pair: ' . $token);
+				}
+				if (Lexer::surroundedBy($keyVal[1], '"', '"'))
+					$kvPairs[$keyVal[0]] = substr($keyVal[1], 1, -1);
+				else {
+					$vToken = Lexer::getVariableToken($origin, $keyVal[1]);
+					$vNode = new VariableNode($vToken->varLevels,
+						$vToken->filters, false, $origin);
+					$kvPairs[$keyVal[0]] = $vNode;
+				}
+			}
+			else {
+				if (Lexer::surroundedBy($token, '"', '"'))
+					$folders[] = substr($token, 1, -1);
+				else {
+					$vToken = Lexer::getVariableToken($origin, $token);
+					$vNode = new VariableNode($vToken->varLevels,
+						$vToken->filters, false, $origin);
+					$folders[] = $vNode;
+				}
+			}
+		}
+		return new UrlNode($folders, $kvPairs);
+	}
+
+}
+
+?>
