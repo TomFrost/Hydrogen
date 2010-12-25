cef97fc0dae73a158fc1eec980676ab7353919ef
TomFrost <tom@frosteddesign.com>
2010-12-24 23:36:45 -0500

    The first argument of a 'url' tag is now required to be a hardcoded folder.  Use '/' to bypass it and make the rest of the arguments dynamic.

--- view/engines/hydrogen/tags/UrlTag.php
+++ view/engines/hydrogen/tags/UrlTag.php
@@ -15,34 +15,45 @@ use hydrogen\view\engines\hydrogen\exceptions\TemplateSyntaxException;
 class UrlTag extends Tag {
 
 	public static function getNode($cmd, $args, $parser, $origin) {
-		$tokens = preg_split('/[\s\/]/', $args, null, PREG_SPLIT_NO_EMPTY);
 		$folders = array();
 		$kvPairs = array();
-		foreach ($tokens as $token) {
-			if (strpos($token, '=') !== false) {
-				$keyVal = explode('=', $token);
-				if (count($keyVal) !== 2 || !$keyVal[0] || !$keyVal[1]) {
-					throw new TemplateSyntaxException(
-						'URL tag parameter in template "' . $origin .
-						'" contains invalid key=value pair: ' . $token);
-				}
-				if (Lexer::surroundedBy($keyVal[1], '"', '"'))
-					$kvPairs[$keyVal[0]] = substr($keyVal[1], 1, -1);
-				else {
-					$vToken = Lexer::getVariableToken($origin, $keyVal[1]);
-					$vNode = new VariableNode($vToken->varLevels,
-						$vToken->filters, false, $origin);
-					$kvPairs[$keyVal[0]] = $vNode;
-				}
-			}
-			else {
-				if (Lexer::surroundedBy($token, '"', '"'))
-					$folders[] = substr($token, 1, -1);
-				else {
-					$vToken = Lexer::getVariableToken($origin, $token);
-					$vNode = new VariableNode($vToken->varLevels,
-						$vToken->filters, false, $origin);
-					$folders[] = $vNode;
+		$tokens = preg_split('/\s/', $args, 2, PREG_SPLIT_NO_EMPTY);
+		if (isset($tokens[0])) {
+			$folders = preg_split('/\//', $tokens[0], null,
+				PREG_SPLIT_NO_EMPTY);
+			if (isset($tokens[1])) {
+				$tokens = preg_split('/[\s\/]/', $tokens[1], null,
+					PREG_SPLIT_NO_EMPTY);
+				foreach ($tokens as $token) {
+					if (strpos($token, '=') !== false) {
+						$keyVal = explode('=', $token);
+						if (count($keyVal) !== 2 || !$keyVal[0] |
+								!$keyVal[1]) {
+							throw new TemplateSyntaxException(
+								'URL tag parameter in template "' . $origin .
+								'" contains invalid key=value pair: ' .
+								$token);
+						}
+						if (Lexer::surroundedBy($keyVal[1], '"', '"'))
+							$kvPairs[$keyVal[0]] = substr($keyVal[1], 1, -1);
+						else {
+							$vToken = Lexer::getVariableToken($origin,
+								$keyVal[1]);
+							$vNode = new VariableNode($vToken->varLevels,
+								$vToken->filters, false, $origin);
+							$kvPairs[$keyVal[0]] = $vNode;
+						}
+					}
+					else {
+						if (Lexer::surroundedBy($token, '"', '"'))
+							$folders[] = substr($token, 1, -1);
+						else {
+							$vToken = Lexer::getVariableToken($origin, $token);
+							$vNode = new VariableNode($vToken->varLevels,
+								$vToken->filters, false, $origin);
+							$folders[] = $vNode;
+						}
+					}
 				}
 			}
 		}
