843d74d21eba13d77d4a9c027c8de835d79002a0
TomFrost <tom@frosteddesign.com>
2010-12-27 22:26:31 -0500

    Adding the md5 filter.

---
+++ view/engines/hydrogen/filters/Md5Filter.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Copyright (c) 2009 - 2010, Frosted Design
+ * All rights reserved.
+ */
+
+namespace hydrogen\view\engines\hydrogen\filters;
+
+use hydrogen\view\engines\hydrogen\Filter;
+
+class Md5Filter implements Filter {
+
+	public static function applyTo($string, $args, $phpfile) {
+		return 'md5(' . $string . ')';
+	}
+
+}
+
+?>
