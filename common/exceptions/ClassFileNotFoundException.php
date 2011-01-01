57c2c56299610ec1e4bf0333f62fe6b87be33de0
TomFrost <tom@frosteddesign.com>
2011-01-01 09:54:06 -0500

    Getting a bit hacky with the PHP error handling process so that the actual file including the nonexistant class will report the error instead of every not-found error coming from hydrogen.inc.php.

---
+++ common/exceptions/ClassFileNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+/*
+* Copyright (c) 2009 - 2011, Frosted Design
+* All rights reserved.
+*/
+
+namespace hydrogen\common\exceptions;
+
+class ClassFileNotFoundException extends \Exception {
+	
+}
+
+?>
