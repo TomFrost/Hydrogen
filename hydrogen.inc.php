e8c7c4e112361d270041406058254bf5977b97c2
TomFrost <tom@frosteddesign.com>
2011-01-01 09:11:11 -0500

    Massively simplifying the autoloading process.

--- hydrogen.inc.php
+++ hydrogen.inc.php
@@ -11,26 +11,12 @@
 namespace hydrogen;
 
 function load($namespace) {
-	$splitpath = explode('\\', $namespace);
-	$path = '';
-	$name = '';
-	$firstword = true;
-	for ($i = 0; $i < count($splitpath); $i++) {
-		if ($splitpath[$i] && !$firstword) {
-			if ($i == count($splitpath) - 1)
-				$name = $splitpath[$i];
-			else
-				$path .= DIRECTORY_SEPARATOR . $splitpath[$i];
-		}
-		if ($splitpath[$i] && $firstword) {
-			if ($splitpath[$i] != __NAMESPACE__)
-				break;
-			$firstword = false;
-		}
-	}
-	if (!$firstword) {
-		$fullpath = __DIR__ . $path . DIRECTORY_SEPARATOR . $name . '.php';
-		return include_once($fullpath);
+	while ($namespace[0] === '\\')
+		$namespace = substr($namespace, 1);
+	if (strpos($namespace, __NAMESPACE__) === 0) {
+		$path = __DIR__ . '/' . str_replace('\\', '/', substr($namespace,
+			strlen(__NAMESPACE__) + 1)) . '.php';
+		return include_once($path);
 	}
 	return false;
 }
