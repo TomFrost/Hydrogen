993c5208d00e04ecc7a6a126f6cab957ce77e216
TomFrost <tom@frosteddesign.com>
2011-01-01 13:24:27 -0500

    Adding the 'linebreaksbr' filter.

---
+++ view/engines/hydrogen/filters/LinebreaksbrFilter.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Copyright (c) 2009 - 2011, Frosted Design
+ * All rights reserved.
+ */
+
+namespace hydrogen\view\engines\hydrogen\filters;
+
+use hydrogen\view\engines\hydrogen\Filter;
+use hydrogen\view\engines\hydrogen\exceptions\TemplateSyntaxException;
+
+class LinebreaksbrFilter implements Filter {
+
+	public static function applyTo($string, $args, &$escape, $phpfile) {
+		if ($escape === true) {
+			$string = 'htmlentities(' . $string . ')';
+			$escape = false;
+		}
+		return 'nl2br(' . $string . ', true)';
+	}
+
+}
+
+?>
