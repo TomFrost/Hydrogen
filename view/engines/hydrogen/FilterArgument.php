e3f6525b26e7a6a95ce75fc297404633d8ff9897
TomFrost <tom@frosteddesign.com>
2011-01-01 22:30:57 -0500

    Escaping single-quotes in FilterArgument string literal values.

--- view/engines/hydrogen/FilterArgument.php
+++ view/engines/hydrogen/FilterArgument.php
@@ -25,7 +25,7 @@ class FilterArgument {
 				$this->data->origin);
 			return $var->getVariablePHP($phpFile);
 		}
-		return "'" . $this->data . "'";
+		return "'" . str_replace("'", '\\\'', $this->data) . "'";
 	}
 }
 
