4f369d762f68b0be87acd92d9aa6ee729ce960d7
TomFrost <tom@frosteddesign.com>
2011-01-02 11:23:45 -0500

    Adding a decimal places argument to filesizefilter.

--- view/engines/hydrogen/filters/FilesizeformatFilter.php
+++ view/engines/hydrogen/filters/FilesizeformatFilter.php
@@ -12,22 +12,32 @@ use hydrogen\view\engines\hydrogen\exceptions\TemplateSyntaxException;
 class FilesizeformatFilter implements Filter {
 
 	public static function applyTo($string, $args, &$escape, $phpfile) {
-		$phpfile->addFunction('fileSizeFormatFilter', array('$size'), <<<'PHP'
+		if (count($args) > 1) {
+			throw new TemplateSyntaxException(
+				'The "filesizeformat" filter requires one or no arguments.');
+		}
+		$phpfile->addFunction('fileSizeFormatFilter',
+				array('$size', '$decimals=2'), <<<'PHP'
 			$size = (int)$size;
 			if (($size = $size / 1024) < 1024){
-				$size = number_format($size, 2);
+				$size = number_format($size, $decimals);
 				return $size . ' KB';
 			} else if (($size = $size / 1024) < 1024) {
-				$size = number_format($size, 2);
+				$size = number_format($size, $decimals);
 				return $size . ' MB';
 			} else if (($size = $size / 1024) < 1024) {
-				$size = number_format($size, 2);
+				$size = number_format($size, $decimals);
 				return $size . ' GB';
 			}
 PHP
 		);
 		$escape = false;
-		return 'fileSizeFormatFilter(' . $string . ')';
+		if (count($args) === 0)
+			return 'fileSizeFormatFilter(' . $string . ')';
+		else {
+			return 'fileSizeFormatFilter(' . $string . ', ' .
+				$args[0]->getValue($phpfile) . ')';
+		}
 	}
 
 }
