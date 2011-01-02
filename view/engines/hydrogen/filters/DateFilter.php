2f6bebfed3657a9b94da3520bf6f640b682edde8
TomFrost <tom@frosteddesign.com>
2011-01-01 23:01:11 -0500

    Adding the 'date' filter.

---
+++ view/engines/hydrogen/filters/DateFilter.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Copyright (c) 2009 - 2011, Frosted Design
+ * All rights reserved.
+ */
+
+namespace hydrogen\view\engines\hydrogen\filters;
+
+use hydrogen\view\engines\hydrogen\Filter;
+use hydrogen\view\engines\hydrogen\exceptions\TemplateSyntaxException;
+
+class DateFilter implements Filter {
+
+	public static function applyTo($string, $args, &$escape, $phpfile) {
+		if (count($args) !== 1) {
+			throw new TemplateSyntaxException(
+				'The "date" filter requires exactly one argument.');
+		}
+		return '(($dateFilter = strtotime(' . $string . ')) ? date(' .
+			$args[0]->getValue($phpfile) . ', $dateFilter) : ' . $string . ')';
+	}
+
+}
+
+?>
