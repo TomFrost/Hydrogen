2b0757ff5b359962b08df7e6187ecd49e4cf94c9
TomFrost <tom@frosteddesign.com>
2011-01-03 10:03:28 -0500

    Adding the 'urlize' filter.

---
+++ view/engines/hydrogen/filters/UrlizeFilter.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Copyright (c) 2009 - 2011, Frosted Design
+ * All rights reserved.
+ */
+
+namespace hydrogen\view\engines\hydrogen\filters;
+
+use hydrogen\view\engines\hydrogen\Filter;
+use hydrogen\view\engines\hydrogen\exceptions\TemplateSyntaxException;
+
+class UrlizeFilter implements Filter {
+
+	public static function applyTo($string, $args, &$escape, $phpfile) {
+		$phpfile->addFunction('urlizeFilter', array('$str', '$esc'), <<<'PHP'
+			if ($esc)
+				$str = htmlentities($str);
+			$str = preg_replace(
+				'#(https?://[a-zA-Z0-9~\#\-\?\&_/]+(([.=;][a-zA-Z0-9~\#\-\?\&_/]+)+)?)#',
+				'<a href="$0">$0</a>', $str);
+			if ($esc) {
+				$pos = -1;
+				while (($pos = strpos($str, '<', $pos + 1)) !== false) {
+					$end = strpos($str, '>', $pos) + 1;
+					$sub = substr($str, $pos, $end - $pos);
+					$fix = str_replace('&amp;', '&', $sub);
+					$str = substr($str, 0, $pos) . $fix . substr($str, $end);
+				}
+			}
+			return $str;
+PHP
+		);
+		$string = 'urlizeFilter(' . $string . ', ' .
+			($escape ? 'true' : 'false') . ')';
+		$escape = false;
+		return $string;
+	}
+
+}
+
+?>
