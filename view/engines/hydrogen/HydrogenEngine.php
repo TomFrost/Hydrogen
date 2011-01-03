16c151a7ba9bb3d8b050d40e73d419e23cc9f026
TomFrost <tom@frosteddesign.com>
2011-01-03 18:20:35 -0500

    Adding support for adding externals to the engine.

--- view/engines/hydrogen/HydrogenEngine.php
+++ view/engines/hydrogen/HydrogenEngine.php
@@ -6,10 +6,41 @@
 
 namespace hydrogen\view\engines\hydrogen;
 
+use hydrogen\config\Config;
 use hydrogen\view\TemplateEngine;
 use hydrogen\view\engines\hydrogen\Parser;
 
 class HydrogenEngine implements TemplateEngine {
+	
+	protected static $filterClass = array();
+	protected static $filterPath = array();
+	protected static $filterNamespace = array(
+		'\hydrogen\view\engines\hydrogen\filters\\'
+		);
+	protected static $tagClass = array();
+	protected static $tagPath = array();
+	protected static $tagNamespace = array(
+		'\hydrogen\view\engines\hydrogen\tags\\'
+		);
+
+	public static function addFilter($filterName, $className, $path=false) {
+		static::$filterClass[$filterName] = $className;
+		if ($path)
+			static::$filterPath[$filterName] = Config::getAbsolutePath($path);
+	}
+	
+	public static function addFilterNamespace($namespace) {
+		if ($namespace[0] !== '\\')
+			$namespace = '\\' . $namespace;
+		if ($namespace[strlen($namespace) - 1] !== '\\')
+			$namespace .= '\\';
+	}
+	
+	public static function addTag($tagName, $className, $path=false) {
+		static::$tagClass[$tagName] = $className;
+		if ($path)
+			static::$tagPath[$tagName] = Config::getAbsolutePath($path);
+	}
 
 	public static function getPHP($templateName, $loader) {
 		$parser = new Parser($templateName, $loader);
