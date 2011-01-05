6ab6c598b63a3c4a163d7dfd5db96e9044030a68
TomFrost <tom@frosteddesign.com>
2011-01-04 22:55:23 -0500

    Adding the documentation for Token.

--- view/engines/hydrogen/Token.php
+++ view/engines/hydrogen/Token.php
@@ -6,12 +6,25 @@
 
 namespace hydrogen\view\engines\hydrogen;
 
+/**
+ * The Token abstract defines a token that the Hydrogen Templating Engine's
+ * {@link \hydrogen\view\engines\hydrogen\Lexer} can break a template's
+ * contents into.
+ */
 abstract class Token {
 	const TOKEN_TYPE = 0;
 
 	public $origin;
 	public $raw;
 
+	/**
+	 * Creates a new instance of the Token class.
+	 *
+	 * @param string $origin The name of the template from which this token
+	 * 		was generated.
+	 * @param string $raw The raw text of this token, not parsed or modified
+	 * 		in any way.
+	 */
 	public function __construct($origin, $raw) {
 		$this->origin = &$origin;
 		$this->raw = &$raw;
