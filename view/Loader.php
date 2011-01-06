2b0aedc8c8f4677b5e985048cd4d10c4d97d0a04
TomFrost <tom@frosteddesign.com>
2011-01-05 23:00:45 -0500

    Adding the documentation for Loader.

--- view/Loader.php
+++ view/Loader.php
@@ -6,7 +6,28 @@
 
 namespace hydrogen\view;
 
+/**
+ * The Loader interface defines a class is that capable of loading the contents
+ * of a template from some source; whether that source be a file, a database,
+ * or some other storage system.
+ *
+ * Loaders should be placed in the hydrogen/view/loaders folder, declared
+ * under the \hydrogen\view\loaders namespace, and named ______Loader,
+ * where the blank is the name of the loader with a capital first letter.  They
+ * should implement this interface and its method, then can be selected for use
+ * in the hydrogen.autoconfig.php file.
+ */
 interface Loader {
+	
+	/**
+	 * Loads the contents of the specified template.
+	 *
+	 * @param string $templateName The name of the template to be loaded.
+	 * @return string The unaltered, unparsed contents of the specified
+	 * 		template.
+	 * @throws hydrogen\view\exceptions\NoSuchViewException if the specified
+	 * 		template is not found or cannot be loaded.
+	 */
 	public function load($templateName);
 }
 
