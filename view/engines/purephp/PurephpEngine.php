3e2e9ca17432dc4b9955e4dee4de60786e0d09b5
TomFrost <tom@frosteddesign.com>
2011-01-05 23:30:26 -0500

    Adding the documentation for PurephpEngine.

--- view/engines/purephp/PurephpEngine.php
+++ view/engines/purephp/PurephpEngine.php
@@ -8,6 +8,18 @@ namespace hydrogen\view\engines\purephp;
 
 use hydrogen\view\TemplateEngine;
 
+/**
+ * The purephp engine is the simplest possible templating engine for Hydrogen:
+ * the code is literally pure PHP code, and it uses the variables and methods
+ * provided by the {@link \hydrogen\view\ViewSandbox} in order to generate
+ * its output.
+ *
+ * Documentation and examples on this format can be found in the Hydrogen
+ * Overview.
+ *
+ * @link http://www.webdevrefinery.com/forums/topic/1440-hydrogen-overview/
+ *		Hydrogen Overview
+ */
 class PurephpEngine implements TemplateEngine {
 
 	public static function getPHP($templateName, $loader) {
