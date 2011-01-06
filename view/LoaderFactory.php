ba9ae9c5b9ce3a740f3bcf1e91e7dc5f32853cd8
TomFrost <tom@frosteddesign.com>
2011-01-05 23:18:40 -0500

    Adding the documentation for LoaderFactory.

--- view/LoaderFactory.php
+++ view/LoaderFactory.php
@@ -8,11 +8,30 @@ namespace hydrogen\view;
 
 use hydrogen\config\Config;
 
+/**
+ * LoaderFactory is a statically-accessed class that manages the creation of
+ * {@link \hydrogen\view\Loader} objects.  It allows one Loader of each type to
+ * be created, but should a type be requested multiple times, the original
+ * instance will be returned each time.
+ */
 class LoaderFactory {
 	protected static $loaders = array();
 	
+	/**
+	 * LoaderFactory should never be instantiated.
+	 */
 	protected static function __construct() {}
 	
+	/**
+	 * Gets an instance (newly created or old) of a Loader.
+	 *
+	 * @param string $loaderType The type of loader (loader name, with a
+	 * 		capital first letter) of which to get an instance.  This argument
+	 * 		is optional; if false, the loader type specified in the
+	 * 		[view]=>loader config value will be used.  This value is normally
+	 * 		specified within hydrogen.autoload.php.
+	 * @return \hydrogen\view\Loader a Loader of the appropriate type.
+	 */
 	public static function getLoader($loaderType=false) {
 		if (!$loaderType)
 			$loaderType = Config::getVal("view", "loader") ?: "File";
