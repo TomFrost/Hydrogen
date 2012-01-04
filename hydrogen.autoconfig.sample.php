423072a6e88e3ecb7b3fff100df30fa55badfae4
Tom Frost <tom@frosteddesign.com>
2012-01-04 13:53:32 -0500

    All: Bump copyright year.

--- hydrogen.autoconfig.sample.php
+++ hydrogen.autoconfig.sample.php
@@ -1,6 +1,6 @@
 <?php
 /***
- ***  Copyright (c) 2009 - 2011, Frosted Design
+ ***  Copyright (c) 2009 - 2012, Frosted Design
  ***  All rights reserved.
  ***
  ***********************************************
